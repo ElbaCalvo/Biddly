309da3ecf3c179228ed40e583ea608b70a63947a
AngelCarballlo <Angelcarballodaw@gmail.com>
2024-11-08 12:57:04 +0100

    Comentarios y barra naranja

--- app/view/mainScreen.php
+++ app/view/mainScreen.php
@@ -7,12 +7,8 @@
     <link rel="stylesheet" href="mainScreen.css">
 </head>
 <body>
-<<<<<<< Updated upstream
-    <header class="topBar">  
-=======
     <!-- Logo, barra de busqueda y botones de inicio de sesión y registro. -->
     <header class="topBar">
->>>>>>> Stashed changes
         <div class="logoContainer">
             <a href="mainScreen.php"><img class="logo" src="../../img/logoText.png" alt="Logo Biddly"></a>
         </div>
@@ -32,17 +28,6 @@
         <a href=""><li>Vehículo</li></a>
     </nav>
 
-<<<<<<< Updated upstream
-    <div class="contentContainer">
-        <div class="comunityFavorites">Favoritos de la comunidad</div>
-        <img src="../../img/mando.png" alt="Xbox Elite Controller">
-        <div class="price">50€</div>
-        <div class="productName">XBOX ELITE 2 Core Edition</div>
-        <button class="bidButton">Pujar</button>
-        <div class="description">
-            <strong>Descripción</strong><br>
-            <p>descripción descripción descripción descripción descripción descripción descripción descripción descripción descripción descripción...</p>
-=======
     <!-- seccion de Favoritos -->
     <div class="favoritesContainer">
         <div class="contentContainer">
@@ -58,7 +43,6 @@
             <div class="bidTime">
                 3 Dec. 2024, 08:41
             </div>
->>>>>>> Stashed changes
         </div>
         <div class="bidTime">
             3 Dec. 2024, 08:41
