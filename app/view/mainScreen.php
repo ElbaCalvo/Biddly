6a28b28c562ea4b763fc22b6c0fda79ded319e65
AngelCarballlo <Angelcarballodaw@gmail.com>
2024-11-08 12:56:08 +0100

    Prueba mover cambios de main a rama

--- app/view/mainScreen.php
+++ app/view/mainScreen.php
@@ -7,7 +7,12 @@
     <link rel="stylesheet" href="mainScreen.css">
 </head>
 <body>
+<<<<<<< Updated upstream
     <header class="topBar">  
+=======
+    <!-- Logo, barra de busqueda y botones de inicio de sesión y registro. -->
+    <header class="topBar">
+>>>>>>> Stashed changes
         <div class="logoContainer">
             <a href="mainScreen.php"><img class="logo" src="../../img/logoText.png" alt="Logo Biddly"></a>
         </div>
@@ -16,6 +21,8 @@
         <a href="signInScreen.php"><img class="topBarButtons" src="../../img/signin.png" alt=""></a>
         <a href="registerScreen.php"><img class="topBarButtons" src="../../img/register.png" alt=""></a>
     </header>
+
+    <!-- navegacion por Categorias -->
     <nav>
         <a href=""><li class="selected">Todos</li></a>
         <a href=""><li>Ropa</li></a>
@@ -25,6 +32,7 @@
         <a href=""><li>Vehículo</li></a>
     </nav>
 
+<<<<<<< Updated upstream
     <div class="contentContainer">
         <div class="comunityFavorites">Favoritos de la comunidad</div>
         <img src="../../img/mando.png" alt="Xbox Elite Controller">
@@ -34,11 +42,31 @@
         <div class="description">
             <strong>Descripción</strong><br>
             <p>descripción descripción descripción descripción descripción descripción descripción descripción descripción descripción descripción...</p>
+=======
+    <!-- seccion de Favoritos -->
+    <div class="favoritesContainer">
+        <div class="contentContainer">
+            <div class="comunityFavorites">Favoritos de la comunidad</div>
+            <img src="../../img/mando.png" alt="Xbox Elite Controller">
+            <div class="price">50€</div>
+            <div class="productName">XBOX ELITE 2 Core Edition</div>
+            <button class="bidButton">Pujar</button>
+            <div class="description">
+                <strong>Descripción</strong><br>
+                <p>descripción descripción descripción descripción descripción descripción descripción descripción descripción descripción descripción...</p>
+            </div>
+            <div class="bidTime">
+                3 Dec. 2024, 08:41
+            </div>
+>>>>>>> Stashed changes
         </div>
         <div class="bidTime">
             3 Dec. 2024, 08:41
         </div>
     </div>
+
+    <!-- Barra naranja de separacion -->
+    <div class="orangeLine"></div>
 </body>
 </html>
 
