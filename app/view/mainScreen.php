c3c4f8ea6cf31d395111f7a937c8697981db41b5
AngelCarballlo <Angelcarballodaw@gmail.com>
2024-11-11 10:03:26 +0100

    Creado apartado favoritos

--- app/view/mainScreen.php
+++ app/view/mainScreen.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Biddly</title>
     <link rel="stylesheet" href="mainScreen.css">
 </head>
 
