c8246e3a4a1eb2088eb6ea6928660c0715af27e1
Adrian <adriandiazsaiz@gmail.com>
2024-11-25 11:37:13 +0100

    Crear el modelo para las pujas

---
+++ app/model/bid.php
@@ -0,0 +1,47 @@
+<?php
+require_once "../../config/dbConnection.php";
+
+class Bid {
+    private $id;
+    private $producto;
+    private $comprador;
+    private $precio;
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setProducto($producto) {
+        $this->producto = $producto;
+    }
+
+    public function setComprador($comprador) {
+        $this->comprador = $comprador;
+    }
+
+    public function setPrecio($precio) {
+        $this->precio = $precio;
+    }
+
+    public function addBid() {
+        $conn = getDBConnection();
+        $sql = $conn->prepare('INSERT INTO pujas (Producto,Comprador,Precio) VALUES (?,?,?)');
+        $sql->bindParam(1, $this->producto);
+        $sql->bindParam(2, $this->comprador);
+        $sql->bindParam(3, $this->precio);
+        return $sql->execute();
+    }
+
+    public function getBidsByProduct($productId) {
+        try {
+            $conn = getDBConnection();
+            $sql = $conn->prepare('SELECT * FROM pujas WHERE Producto = :producto');
+            $sql->bindParam(':producto', $productId);
+            $sql->execute();
+            return $sql->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo 'Error: ' . $e->getMessage();
+            return null;
+        }
+    }
+}
