d81c3a9d34632b59a2010489cb9003d059f6c09d
AngelCarballlo <Angelcarballodaw@gmail.com>
2024-12-02 18:02:42 +0100

    Saneamiento de campos en addProduct y fecha actual como valor minimo

--- app/view/addProductScreen.php
+++ app/view/addProductScreen.php
@@ -15,20 +15,26 @@
     $productController = new ProductController();
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['addProductForm'])) {
-        $nombre = $_POST['nombre'];
+
+        $nombre = filter_input(INPUT_POST, 'nombre', FILTER_SANITIZE_STRING);
         $precio = $_POST['precio'];
+        $descripcion = filter_input(INPUT_POST, 'descripcion', FILTER_SANITIZE_STRING);
+        $img = filter_input(INPUT_POST, 'url', FILTER_SANITIZE_URL);
         $categoria = $_POST['categoria'];
-        $descripcion = $_POST['descripcion'];
-        $img = $_POST['url'];
         $vendedor = $_SESSION['usuario'];
         $fecha = $_POST['fecha'];
 
+        if ($nombre && $precio && $categoria && $descripcion && $img && $vendedor && $fecha){
         $product_id = $productController->addProduct($nombre, $descripcion, $categoria, $precio, $img, $fecha, $vendedor);
 
-        echo "<script>alert('Producto registrado con exito.')</script>";
-        header("Location: biddScreen.php?product_id=$product_id");
-        exit();
-    }
+        if ($product_id) {
+            header("Location: biddScreen.php?product_id=$product_id");
+            exit();
+        } else {
+            echo "<script>alert('Error al registrar el producto en la base de datos.')</script>";
+        }
+    } 
+}
 
     ?>
         <!-- Logo, barra de busqueda y botones de inicio de favoritos y usuario. -->
@@ -62,7 +68,7 @@
                 <input type="text" name="nombre" id="name" placeholder="Mandos XBOX" required>
 
                 <label>Precio del producto</label>
-                <input type="number" name="precio" id="price" placeholder="50€" required>
+                <input type="number" name="precio" id="price" placeholder="50€" min=1 required>
 
                 <label>Categoría</label>
                 <select name="categoria" id="categoria" required>
@@ -79,7 +85,7 @@
                 <input type="text" id="url" name="url" placeholder="https://safetypricelectronics.com/spcontents/u/2022/07/games-g664c6d5aa_1920.jpg" required>
 
                 <label>Fecha de la subasta</label>
-                <input type="datetime-local" name="fecha" id="date" placeholder="14/06/2024, 22:00" required>
+                <input type="datetime-local" name="fecha" id="date" min=<?php echo date('Y-m-d\TH:i') ?> required> <!-- \T es la separacion y H:i la hora actual -->
 
                 <label>Descripción del producto</label>
                 <input type="text" name="descripcion" id="description" placeholder="descripción" required>
